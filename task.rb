2767a9885cb03d7a605667c1669c3d8a0586f063
Rika Sue <su_tff@icloud.com>
2021-09-29 22:19:01 +0900

    zsh:1: command not found: :wq

--- task.rb
+++ task.rb
@@ -5,7 +5,7 @@ def q1
  names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
- names.push("斎藤")
+ names.<<("斎藤")
  p names
 end
 
@@ -29,7 +29,7 @@ def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
-  sports.delete(nil)
+  sports.compact!
   # 以下は変更しないで下さい
   p sports
 end
@@ -39,20 +39,23 @@ def q5
   array2 = [1, 5, 8, 10]
 
   # 以下に回答を記載
-
+  p array1.empty?
+  p array2.empty?
 end
 
 def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
-
+  numbers2 = numbers1.map {|n| n*10}
+  p numbers2
 end
 
 def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
+  array.map!{|x| x.to_i}
 
   # 以下は変更しないで下さい
   p array
@@ -62,6 +65,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
+  programming_languages.map!{|x| x.capitalize}
+  programming_languages.map!{|x| x.upcase}
 
   # 以下は変更しないで下さい
   p programming_languages
